41fde32ef1285701e8e4c4c83cb46bef22835fea
yudaprawira <yuda.prawira4@gmail.com>
2018-07-31 17:03:30 +0700

    update namespace

--- src/UploadVideo/Uploader.php
+++ src/UploadVideo/Uploader.php
@@ -1,5 +1,5 @@
 <?php
-namespace UploadVideo;
+namespace Yuda\UploadVideo;
 
 /**
  * @author yuda KLN
@@ -10,7 +10,7 @@ class Uploader
 {
     function init()
     {
-        echo 'test';
+        echo 'test namespace';
     }
 }
 
